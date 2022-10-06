547430ea5a2e91d1bc6eaf573f14ddece08603c1
macwinnie <dev@macwinnie.me>
2022-10-06 14:20:30 +0200

    update documentation

--- setup.py
+++ setup.py
@@ -28,7 +28,9 @@ setup(
     ],
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    python_requires=">={pyVersion}".format(pyVersion=pf["_meta"]["requires"]["python_version"]),
+    python_requires=">={pyVersion}".format(
+        pyVersion=pf["_meta"]["requires"]["python_version"]
+    ),
     install_requires=[
         "{package}{version}".format(package=p, version=v) if v != "*" else p
         for p, v in pf["default"].items()
